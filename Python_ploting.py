1c9243407bcd3368fd9d03024b7835bf22d3f8fe
Sezin Yaman <musahedat1@gmail.com>
2019-02-15 14:12:01 +0200

    Tweaks

--- Python_ploting.py
+++ Python_ploting.py
@@ -1,21 +1,17 @@
 
 # coding: utf-8
 
-# In[2]:
 
 import pandas as pd
 import os
 from matplotlib import pyplot as plt
 
 
-# In[3]:
-
-os.chdir("/Users/Yaman/Desktop/Python")
+os.chdir("/Users/Desktop/Python")
 df = pd.read_csv("clus_data.csv")
 df.info();
 
 df.info()
-# In[26]:
 
 # plt.plot(df.M1, df.USERB);
 # let's add some legend
@@ -33,41 +29,28 @@ plt.show()
 #'classic' - The default color scheme for Matplotlib
 
 
-# In[28]:
-
 plt.style.use("ggplot")
 plt.plot(df.JOBTIME, df.USERC, label="USERC", linestyle=':', marker='s', color="DarkCyan");
 plt.legend()
 plt.show()
 
 
-# In[38]:
-
 plt.style.use("seaborn")
 plt.plot(df.JOBTIME, df.USERC, label="USERC", linestyle=':', marker='s', color="DarkCyan");
 plt.legend()
 plt.show()
 
-
-# In[30]:
-
 # to see the all options:
 print(plt.style.available)
 
-
-# In[44]:
 
 plt.scatter(df.USERB, df.USERC, color="red", alpha=0.1)
 plt.show()
 
 
-# In[60]:
-
 plt.bar(df['USERB'], df['worktime'], color="red")
 plt.show()
 
-
-# In[92]:
 
 # range sets the minimum and maximum datapoints that we will include in our histogram.
 # bins sets the number of points in our histogram.
@@ -75,9 +58,5 @@ plt.show()
 
 plt.hist(df.JOBTIME, range= (1, 120), bins=20, normed=True)
 plt.show()
-
-
-# In[ ]:
-
 
 
