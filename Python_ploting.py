d90d5c3e365ded2b787c9bc33dfe448f59774dba
Sezin Yaman <musahedat1@gmail.com>
2019-02-15 14:10:57 +0200

    Different ways of plotting

---
+++ Python_ploting.py
@@ -0,0 +1,83 @@
+
+# coding: utf-8
+
+# In[2]:
+
+import pandas as pd
+import os
+from matplotlib import pyplot as plt
+
+
+# In[3]:
+
+os.chdir("/Users/Yaman/Desktop/Python")
+df = pd.read_csv("clus_data.csv")
+df.info();
+
+df.info()
+# In[26]:
+
+# plt.plot(df.M1, df.USERB);
+# let's add some legend
+plt.style.use("fivethirtyeight")
+plt.plot(df.JOBTIME, df.USERB, label="USERB", color="Orange");
+plt.plot(df.JOBTIME, df.USERC, label="USERC", linestyle=':', marker='s');
+# add y-axis label
+plt.ylabel("USERB answers")
+plt.legend()
+plt.show()
+# more on styles:
+#'fivethirtyeight' - Based on the color scheme of the popular website
+#'grayscale' - Great for when you don't have a color printer!
+#'seaborn' - Based on another Python visualization library
+#'classic' - The default color scheme for Matplotlib
+
+
+# In[28]:
+
+plt.style.use("ggplot")
+plt.plot(df.JOBTIME, df.USERC, label="USERC", linestyle=':', marker='s', color="DarkCyan");
+plt.legend()
+plt.show()
+
+
+# In[38]:
+
+plt.style.use("seaborn")
+plt.plot(df.JOBTIME, df.USERC, label="USERC", linestyle=':', marker='s', color="DarkCyan");
+plt.legend()
+plt.show()
+
+
+# In[30]:
+
+# to see the all options:
+print(plt.style.available)
+
+
+# In[44]:
+
+plt.scatter(df.USERB, df.USERC, color="red", alpha=0.1)
+plt.show()
+
+
+# In[60]:
+
+plt.bar(df['USERB'], df['worktime'], color="red")
+plt.show()
+
+
+# In[92]:
+
+# range sets the minimum and maximum datapoints that we will include in our histogram.
+# bins sets the number of points in our histogram.
+# Normalizing the histogram so that the sum of the bins adds to 1 with the density(or normed) keyword.
+
+plt.hist(df.JOBTIME, range= (1, 120), bins=20, normed=True)
+plt.show()
+
+
+# In[ ]:
+
+
+
